20f6db9cbfb1f7ff67e5827b66bdb2f67beb5c3f
root <root@localhost>
2014-10-10 10:23:17 +0800

    new version

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #include "delegate.h"
 
-delegate(void, th, (int i), (i));
+delegate_m(void, th, int);
 extern void Call(th delegate_method);
 
 class A
@@ -10,14 +10,22 @@ class A
 public:
 	void p(int i)
 	{
-		cout << "delegate print:" << i << endl;
+		cout << "[class]delegate print:" << i << endl;
 	}
 };
+
+void p(int i)
+{
+	cout << "[func]delegate print:" << i << endl;
+}
 
 int main()
 {
 	A t;
 	th delegate_0(&t, &A::p);
 	Call(delegate_0);
+	Call(&p);
+	Call({&t, &A::p});
+	delegate_0(789);
 	return 0;
 }
