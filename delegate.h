d1ae8975843164cb3fbe8af3a788d6ae7082a5aa
root <root@localhost>
2014-10-10 10:23:31 +0800

    new version

--- delegate.h
+++ delegate.h
@@ -1,36 +1,57 @@
 #pragma once
 
-#define delegate(ret,name,paras,call_paras) \
-class name \
-{ \
-private: \
-	class delegate_base \
-	{ \
-	private: \
-		ret (*sptr) paras; \
-	public: \
-		delegate_base() : sptr(nullptr) { } \
-		delegate_base(ret (*Func) paras) : sptr(Func) { } \
-		virtual ret Invoke paras { return (*this->sptr) call_paras; } \
-		virtual delegate_base *clone() { return new delegate_base(this->sptr); } \
-		virtual ~delegate_base() { this->sptr = nullptr; } \
-	}; \
-	template<typename T> class delegate_##name : public delegate_base \
-	{ \
-	private: \
-		ret (T::*fptr) paras; \
-		T *cptr; \
-	public: \
-		delegate_##name (T *Obj, ret (T::*Func) paras) { this->cptr = Obj; this->fptr = Func; } \
-		delegate_base *clone() override { return new delegate_##name (this->cptr, this->fptr); } \
-		ret Invoke paras override { return (cptr->*fptr) call_paras; } \
-		virtual ~delegate_##name () { this->cptr = nullptr; this->fptr = nullptr; } \
-	}; \
-	delegate_base *delegate_ptr; \
-public: \
-	name (ret (*Func) paras) { this->delegate_ptr = new delegate_base(Func); } \
-	template<typename T> name (T *Obj, ret (T::*Func) paras) { this->delegate_ptr = new delegate_##name<T>(Obj, Func); } \
-	name (name &p) { delegate_ptr = p.delegate_ptr->clone(); } \
-	ret operator() paras { this->delegate_ptr->Invoke call_paras; } \
-	~name () { delete this->delegate_ptr; } \
-}
+#define delegate_m(ret_,name,...) using name = delegate<ret_,##__VA_ARGS__>;
+
+template<typename ret, typename ... paras>
+class delegate
+{
+private:
+	class _tmp { virtual ret tfunc(paras ...) ; };
+	template<typename T>
+	class delegate_struct
+	{
+	private:
+		ret (*sptr) (paras ...);
+		ret (T::*fptr) (paras ...);
+		T *cptr;
+	public:
+		delegate_struct(ret (*Func)(paras ...)) : sptr(Func), fptr(nullptr), cptr(nullptr) { }
+		delegate_struct(T *Obj, ret (T::*Func)(paras ...)) : sptr(nullptr), fptr(Func), cptr(Obj) { }
+		ret Invoke (paras ... args)
+		{
+			return (sptr != nullptr)? (*sptr)(args ...) : (cptr->*fptr)(args ...);
+		}
+		~delegate_struct()
+		{
+			this->cptr = nullptr;
+			this->fptr = nullptr;
+		}
+	};
+	delegate_struct<_tmp> *delegate_ptr;
+public:
+	delegate(ret (*Func)(paras ...))
+	{
+		this->delegate_ptr = new delegate_struct<_tmp>(Func);
+	}
+	template<typename T>
+	delegate(T *Obj, ret (T::*Func)(paras ...))
+	{
+		this->delegate_ptr = (decltype(this->delegate_ptr))new delegate_struct<T>(Obj, Func);
+	}
+	delegate(const delegate &p)
+	{
+		delegate_ptr = new delegate_struct<_tmp>(*p.delegate_ptr);
+	}
+	ret operator()(paras ... args)
+	{
+		return this->Invoke(args ...);
+	}
+	inline ret Invoke(paras ... args)
+	{
+		return this->delegate_ptr->Invoke(args ...);
+	}
+	~delegate()
+	{
+		delete this->delegate_ptr;
+	}
+};
